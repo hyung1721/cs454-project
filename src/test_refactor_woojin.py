7a48da9db804dfa41b385796ef42baa85c06e84d
ShinDongHwan <sdhqaa@naver.com>
2024-11-27 21:33:27 +0900

    test_refactor_woojin 롤백

--- src/test_refactor_woojin.py
+++ src/test_refactor_woojin.py
@@ -3,19 +3,12 @@ from random import choice
 
 from src.core.parsing import parse_library
 from src.core.refactor import REFACTORING_TYPES
-from collections import namedtuple
-import eval.metrics as mt
-
-Metric_Log = namedtuple('Metric_Log', ['before', 'after'])
 
 if __name__ == '__main__':
     node_container_dict = parse_library("./target_libraries/library_example1")
 
     # collect all classes from library
     classes = []
-    # Metric Eval
-    metric_types = mt.get_metric_types_in_paper()
-
     for file_path, node_container in node_container_dict.items():
         for idx, node in enumerate(node_container.nodes):
             if isinstance(node, ast.ClassDef):
@@ -28,8 +21,6 @@ if __name__ == '__main__':
     _refactoring = choice(REFACTORING_TYPES)
     print(target_class_location, _refactoring)
 
-    metric_values_before = mt.calculate_metrices(target_class_location, metric_types)
-
     refactor = _refactoring(base=node_container_dict, location=target_class_location)
 
     if refactor.is_possible():
@@ -38,24 +29,8 @@ if __name__ == '__main__':
         for item in refactor.result.values():
             for node in item.nodes:
                 print(ast.unparse(node))
-   
-        metric_values_after = mt.calculate_metrices(refactor.result.values(), metric_types)
-        better_metric = {}
-        stable_metric = {}
-        worse_metric = {}
 
-        # TODO: 비교 과정 자체도 함수화 - 신동환, 2024-11-27
-        for metric_value_type in metric_values_after:
-            metric_value_before = metric_values_before[metric_value_type]
-            metric_value_after = metric_values_after[metric_value_type]
-            if metric_value_after == metric_value_before: #오차율 0.1%까지 고려해야할지도?
-                stable_metric[metric_value_type] = Metric_Log(metric_value_before, metric_value_after)
-            elif mt.evaluate_improvement(metric_value_type, metric_value_before, metric_value_after):
-                better_metric[metric_value_type] = Metric_Log(metric_value_before, metric_value_after)
-            else:
-                worse_metric[metric_value_type] = Metric_Log(metric_value_before, metric_value_after)
-        
-        # TODO: metric 계산 & metric이 오르지 않으면 undo   
+        # TODO: metric 계산 & metric이 오르지 않으면 undo
         # refactor.undo()
         #
         # print("##### After undo #####")
