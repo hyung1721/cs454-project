53dfff50fb2fcf688bd55e0c5c1006ee0ba6cdfc
ShinDongHwan <sdhqaa@naver.com>
2024-12-01 19:56:24 +0900

    아마도 대충 돌아감 version

--- main.py
+++ main.py
@@ -39,23 +39,23 @@ def make_statistics(result_logs: List[Iteration_Result], metric_type_list):
     statistics: Dict[MetricType, Statistics_Unit] = {}
     
     for metric_type in metric_type_list:
-        statistics[metric_type] = Statistics_Unit(0, 0, 0)
+        statistics[metric_type] = [0, 0, 0]
 
     for result_log in result_logs:
         for metric_type in metric_type_list:
             if metric_type in result_log.better_metric:
-                statistics[metric_type].better_count+=1
+                statistics[metric_type][0]+=1
             elif metric_type in result_log.static_metric:
-                statistics[metric_type].static_count+=1
+                statistics[metric_type][1]+=1
             elif metric_type in result_log.worse_metric:
-                statistics[metric_type].worse_count+=1
+                statistics[metric_type][2]+=1
     return statistics
 
 def fitness_function_improves(iteration_result: Iteration_Result):
-    return not iteration_result.better_metric
+    return len(iteration_result.better_metric) > 0
 
 def is_finish_cycle(refactoring_count):
-    return refactoring_count < constant.DESIRED_REFACTORING_COUNT
+    return refactoring_count >= constant.DESIRED_REFACTORING_COUNT
 
 # Main Function
 if __name__ == '__main__':
@@ -71,23 +71,25 @@ if __name__ == '__main__':
     
     # Paper Algorithm Start
     refactoring_count = 0
+    try_count = 0
 
     metrics_origin = calculate_metrics(node_container_dict, metric_types)
     result_logs: List[Iteration_Result] = []
 
-    while(is_finish_cycle(refactoring_count)):
+    while(is_finish_cycle(refactoring_count) == False):
         print(f"Start New: {refactoring_count}")
         is_first = True
         classes = classes_origin.copy()
-        while(len(classes) > 0):
+        while(len(classes) > 0 and is_finish_cycle(refactoring_count) == False):
             target_class = classes.pop()
             refactoring_methods = REFACTORING_TYPES.copy()
             shuffle(refactoring_methods) # Random
-            while(len(refactoring_methods) > 0):
+            while(len(refactoring_methods) > 0 and is_finish_cycle(refactoring_count) == False):
                 refactoring_method = refactoring_methods.pop()
                 refactor = refactoring_method(base=node_container_dict, location=target_class)
                 if refactor.is_possible():
                     refactor.do()
+                    try_count+=1
                     metrics_before = metrics_origin if is_first else metrics_before
                     metrics_after = calculate_metrics(refactor.result, metric_types)
                     iteration_result = compare_metrics(metrics_before, metrics_after)
@@ -99,8 +101,12 @@ if __name__ == '__main__':
                         print(f"{refactoring_count}th Refactoring_Success: {refactoring_method}")
                     else:
                         refactor.undo()
+                    if(try_count%100 == 0):
+                        print(f"We tried {try_count} times and succeed {refactoring_count} times")
+                        print(f"classes remains {len(classes)} and refactoring_methods remains {len(refactoring_methods)}")
+                
     
     # Print Table3 in Paper
-    statistics = make_statistics(result_logs)
+    statistics = make_statistics(result_logs, metric_types)
     for metric_type, statistic in statistics.items():
-        print(f"{metric_type} {statistic.better_count}↑ {statistic.worse_count}↓")
+        print(f"{metric_type} {statistic[0]}↑ {statistic[1]}= {statistic[2]}↓")
